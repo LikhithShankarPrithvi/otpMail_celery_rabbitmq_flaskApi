5e3b4a73cf5fa3a2e59a8d64c7e8408ec6558d68
LikhithShankarPrithvi <likhithshankarprithvi@gmail.com>
2021-01-06 00:28:04 +0530

    modified mail.py by implementing mimetext

--- mail.py
+++ mail.py
@@ -1,15 +1,23 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.ehlo()
-server.starttls()
-server.login("likhithshankartpt@gmail.com","likhith@tpt")
-message="""From:Illuminati <illuminati2021@gmail.com>
-To:Likhith <likhithshankarprithvi@gmail.com>
-Subject:Finale 
+def send_otp_mail(email,otp):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.login("likhithshankartpt@gmail.com","likhith@tpt")
 
-Trying to test the technology
-I am going to Rock you from rock bottom
-You did see what you are intented to see!!"""
-server.sendmail("likhithshankartpt@gmail.com","likhithshankarprithvi@gmail.com",message)
-server.quit()
+    sender_email = "likhithshankartpt@gmail.com"
+    receiver_email = email
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "OTP"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    text = """Hii There 
+Your required otp is:{0}""".format(otp)
+    part1 = MIMEText(text, "plain")
+    message.attach(part1)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+    server.quit()
