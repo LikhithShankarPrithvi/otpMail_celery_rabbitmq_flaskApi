fd7d4769a0a18810b1662f52e75ba5bb88247637
LikhithShankarPrithvi <likhithshankarprithvi@gmail.com>
2021-01-06 13:09:22 +0530

    created celery_task in flask api
    created celery tasks which are reversing a string and sending otp mail using the flask_celery config module

--- flask_api.py
+++ flask_api.py
@@ -1,16 +1,33 @@
 from flask import Flask
+from tasks import make_celery
 from mail import send_otp_mail
 from otp_gen import otp
 
 app=Flask(__name__)
+app.config['CELERY_BROKER_URL']='pyamqp://'
+app.config['CELERY_RESULT_BACKEND']='rpc://'
 
-@app.route('/')
-def hello():
-    return('Hello World')
+celery=make_celery(app)
+
+@app.route('/<name>')
+def hello(name):
+    reverse.delay(name)
+    return(name)
 
 @app.route('/send/<username>',methods=['GET'])
 def send_mail(username):
-    send_otp_mail(username,otp())
-    return username
+    otp_mail_gen.delay(username,otp())
+    #send_otp_mail(username,otp())
+    return("An email containing 4-digit OTP is sent successfully")
 
-app.run(debug=True)
+@celery.task(name='flask_api.reverse')
+def reverse(string):
+    return string[::-1]
+
+@celery.task(name='flask_api.otp_mail_gen')
+def otp_mail_gen(username,number):
+    send_otp_mail(username,number)
+    return number
+
+if(__name__ == '__main__'):
+    app.run(debug=True)
